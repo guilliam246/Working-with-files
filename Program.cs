39e9879f455602e5e513c2cb31ed2999a57da1c7
Guilliam Oberholster <guilliamoberholster@gmail.com>
2022-06-01 11:50:58 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿  using System;
+  using System.IO;
+
+namespace Working_with_files
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           var path = @"c:\somefile.jpg";
+          File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile\myfile.jpg,", true);
+           File.Delete (path);
+            if (File.Exists (path))
+               {
+              //
+               }
+           var content =  File.ReadAllText(path);
+
+            var fileInfo = new FileInfo (path);
+           fileInfo.CopyTo("...");
+           fileInfo.Delete();
+           if (fileInfo.Exists)
+            {
+                //
+            }
+     
+            Directory.CreateDirectory (@"C:\Users\guill\source\repos");
+            var files =  Directory.GetFiles (@"C:\Users\guill\source\repos", "*.sln", SearchOption.AllDirectories);
+
+            foreach (var file in files)
+            Console.WriteLine(file);
+
+            var directories = Directory.GetDirectories(@"C:\Users\guill\source\repos", "*.sln", SearchOption.AllDirectories);
+            foreach (var directory in directories)
+                Console.WriteLine(directory);
+        }
+    }
+}
